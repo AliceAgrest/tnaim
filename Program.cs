5e767b364639b1710c7500a799da4a11afe7f931
AliceAgrest <alice.agrest@gmail.com>
2022-10-06 10:32:17 +0300

    tnaim1

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        // answer 3
+        /*
+        int num;
+        Console.WriteLine("Enter a numner ");
+        num = int.Parse(Console.ReadLine());
+
+        if ((num < 10) && (num > (-10)))
+            Console.WriteLine("The number you enter is one digt");
+        else
+            Console.WriteLine("The number you enter isn't one digt");
+        */
+        // answer 4
+        char tav ,a ,z ;
+        Console.WriteLine("Enter a charecter ");
+        tav = char.Parse(Console.ReadLine());
+
+            tav = (char)(int)tav;
+            a = (char)(int)'a';
+            z = (char)(int)'z';
+
+        if ((tav >= a) && (tav <= z))
+            Console.WriteLine("Your charecter is in the alphbet");
+        else
+            Console.WriteLine("Your charecter isn't exsit in the alphabet");
+        
+        Console.ReadLine();
+        }
+    }
+}
